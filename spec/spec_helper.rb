44e435a9c2695810b46fb5f1f5ea594369ffeadb
Julie Hsieh <jaykayshay@gmail.com>
2014-09-04 21:51:26 -0700

    Finish layout and routes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,10 @@ Spork.prefork do
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
+  RSpec.configure do |config|
+    config.include Rails.application.routes.url_helpers
+  end
+
   # Checks for pending migrations before tests are run.
   # If you are not using ActiveRecord, you can remove this line.
   ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
